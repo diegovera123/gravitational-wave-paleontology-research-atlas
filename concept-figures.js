64bf42f3ee20b48b9df7d38d3de4e4521c10e2df
Diego Hernandez Vera <121052512+diegovera123@users.noreply.github.com>
2026-09-22 15:13:48 -0400

    Avoid duplicate SVG text class attributes in the natal-kick vector diagram

--- concept-figures.js
+++ concept-figures.js
@@ -31,9 +31,9 @@ function kick(){
  '<circle cx="151" cy="117" r="13" class="figure-star"/><circle cx="267" cy="117" r="8" class="figure-remnant"/>'+
  L(267,117,267,51,"figure-velocity",true)+L(267,117,358,117,"figure-kick",true)+
  L(267,117,358,51,"figure-result",true)+
- T(178,36,"Before collapse")+T(264,45,"v", 'class="figure-text-velocity"')+
- T(314,105,"kick w",'class="figure-text-kick"')+
- T(372,58,"v + w",'class="figure-text-result"')+
+ T(178,36,"Before collapse")+T(264,45,"v")+
+ T(314,105,"kick w",'class="figure-text-kick"'))+
+ T(372,58,"v + w",'class="figure-text-result"'))+
  L(389,111,451,111,"figure-link",true)+
  '<rect x="454" y="70" width="150" height="103" rx="12" class="figure-panel"/>'+
  T(465,95,"New mass M′")+T(465,119,"New separation r")+T(465,143,"New relative speed |v′|")+
